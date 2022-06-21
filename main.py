b019b80ead527de6b79d848d6163683f93db4a7d
tsyganno <tsyganlee@gmail.com>
2022-06-21 22:47:35 +0300

    ready

--- main.py
+++ main.py
@@ -1,1 +1,60 @@
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Автоответчик</title>
+    </head>
+    <body>
+        <h1>Введите сообщение для отправки:</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Отправить</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://localhost:8000/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var messageJson = JSON.parse(event.data);
+                var content = document.createTextNode(messageJson.id + ", " +  messageJson.text)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                var message = {text: input.value};
+                var messageJson = JSON.stringify(message);
+                ws.send(messageJson)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+
+@app.websocket_route("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    count = 1
+    while True:
+        data = await websocket.receive_json()
+        data['id'] = count
+        count += 1
+        await websocket.send_json(data)
+
+
 
